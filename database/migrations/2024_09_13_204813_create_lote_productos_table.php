3ae3ab10dfe1a0f9847fe1dc511153aaeb050256
Rudy Ajuchán <rudyajuchansec32@gmail.com>
2024-09-13 16:21:49 -0600

    tablas y modelos

---
+++ database/migrations/2024_09_13_204813_create_lote_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lote_productos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cantidad');
+            $table->decimal('precio', 8, 2);
+            $table->date('fecha_compra');
+            $table->integer('cantidad_restante');
+            $table->date('fecha_vencimiento');
+            $table->foreignId('productos_id')->constrained('productos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lote_productos');
+    }
+};
---
+++ database/migrations/2024_09_13_210845_create_detalle_compras_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalle_compras', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cantidad');
+            $table->decimal('precio_unitario', 8,2);
+            $table->foreignId('compras_id')->constrained('compras');
+            $table->foreignId('productos_id')->constrained('productos');
+            $table->foreignId('lote_productos_id')->constrained('lote_productos');
+            $table->integer('estado')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_compras');
+    }
+};
